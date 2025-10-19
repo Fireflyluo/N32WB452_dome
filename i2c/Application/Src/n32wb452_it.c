5a579379f14826f5947e4bdc17b93d8c3636b430
fireflyluo <2161486135@qq.com>
2025-10-19 21:12:55 +0800

    refactor(i2c): 重构I2C驱动及相关文件结构
    - 更新main.c文件头注释格式并调整变量声明位置
    - 修改n32wb452_it.c中断服务程序文件注释及描述信息
    - 完善bsp_i2c.h和bsp_i2c.c文件头部注释及版本信息
    - 调整dame.map链接映射文件中的符号引用关系
    - 移除冗余函数调用与未使用代码段
    - 优化I2C轮询测试函数中变量作用域
    - 更新BSP层I2C初始化及相关传输函数的依赖关系

--- i2c/Application/Src/n32wb452_it.c
+++ i2c/Application/Src/n32wb452_it.c
@@ -1,30 +1,17 @@
-/*****************************************************************************
- * Copyright (c) 2019, Nations Technologies Inc.
- *
- * All rights reserved.
- * ****************************************************************************
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the disclaimer below.
- *
- * Nations' name may not be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY NATIONS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
- * DISCLAIMED. IN NO EVENT SHALL NATIONS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * ****************************************************************************/
-
+/*-----------------------------------------------File Info------------------------------------------------
+** File Name:               n32wb452_it.c
+** Last modified date:      2025.10.19
+** Last version:            V0.1
+** Description:             中断服务程序实现
+**
+**--------------------------------------------------------------------------------------------------------            
+** Created date:            2025.10.19
+** author:                  Fireflyluo
+** Version:                 V0.1
+** Descriptions:            实现GPIO外部中断、UART、SPI、DMA、TIM等外设的中断处理函数；
+**                          调用BSP层统一中断处理函数；
+**                          管理空闲帧中断以实现DMA接收数据解析。
+**--------------------------------------------------------------------------------------------------------*/
 /**
  * @file n32wb452_it.c
  * @author Nations
